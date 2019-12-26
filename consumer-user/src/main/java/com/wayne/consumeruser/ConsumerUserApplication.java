3e9e2db60e273675a32987cba43ea45effde9a77
lhj1024 <18875143382@163.com>
2019-12-26 14:13:05 +0800

    服务者和消费者成功注册，并提供服务者service

--- consumer-user/src/main/java/com/wayne/consumeruser/ConsumerUserApplication.java
+++ consumer-user/src/main/java/com/wayne/consumeruser/ConsumerUserApplication.java
@@ -2,12 +2,21 @@ package com.wayne.consumeruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
+@EnableEurekaClient
 @SpringBootApplication
 public class ConsumerUserApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ConsumerUserApplication.class, args);
 	}
-
+	@LoadBalanced //使用负载均衡机制
+	@Bean
+	public RestTemplate restTemplate(){
+		return new RestTemplate();
+	}
 }
